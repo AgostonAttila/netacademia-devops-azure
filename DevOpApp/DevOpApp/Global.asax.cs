f34211ef5cfba67cf52fadf79a3b415219fc3d57
atesz014 <atesz014@gmail.com>
2019-01-10 19:28:06 +0100

    Initial commit

---
+++ DevOpApp/DevOpApp/Global.asax.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Optimization;
+using System.Web.Routing;
+
+namespace DevOpApp
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        private static readonly log4net.ILog log = log4net.LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        protected void Application_Start()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+            using (log4net.ThreadContext.Stacks["NDC"].Push("Application_Start"))
+            {
+                log.DebugFormat("Begin: {0}",this.Context.Server);
+                AreaRegistration.RegisterAllAreas();
+                FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+                RouteConfig.RegisterRoutes(RouteTable.Routes);
+                BundleConfig.RegisterBundles(BundleTable.Bundles);
+            }
+        }
+
+        protected void Application_End()
+        {
+            using (log4net.ThreadContext.Stacks["NDC"].Push("Application_End"))
+            {
+                log.DebugFormat("Begin: {0}", this.Context.Server);
+            }
+        }
+
+        protected void Application_BeginRequest()
+        {
+            using (log4net.ThreadContext.Stacks["NDC"].Push("Application_BeginRequest"))
+            {
+                log.DebugFormat("Begin: {0}", this.Request.RawUrl);
+            }
+        }
+
+        protected void Application_EndRequest()
+        {
+            using (log4net.ThreadContext.Stacks["NDC"].Push("Application_EndRequest"))
+            {
+                log.DebugFormat("Begin: {0}", this.Request.RawUrl);
+            }
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+﻿# Netacademia DevOps Azure-on
+
+https://app.netacademia.hu/Tanfolyam/DEVOPS-azureon
+
+Github megismerése, forráskód letöltése, visualstudio.com bevezetés, git kódtár létrehozásea, a kód feltöltése, branch-ek kialakítása, Build készítése az ASP.NET MVC alkalmazáshoz, Azure WebApp készítése és MVC telepítés automatizálása
+Naplók küldése Azure servicebus-ra, Naplók begyűjtése RabbitMQ-val az ELK szerverre, Napló elemzése
+
+
+
+
+
